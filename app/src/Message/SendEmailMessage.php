0c454ebfca606ece7785aff54b6d9fa392bd501b
Norivo7 <norivo7@gmail.com>
2025-07-11 09:33:27 +0200

    feat: add assert validation for email message

--- app/src/Message/SendEmailMessage.php
+++ app/src/Message/SendEmailMessage.php
@@ -3,14 +3,21 @@ declare(strict_types=1);
 
 namespace App\Message;
 
+use Symfony\Component\Validator\Constraints as Assert;
 use App\Contract\AsyncMessageInterface;
 
 final readonly class SendEmailMessage implements AsyncMessageInterface
 {
 
     public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Email]
         public string $to,
+
+        #[Assert\NotBlank]
         public string $subject,
+
+        #[Assert\NotBlank]
         public string $content
     ) {}
 
