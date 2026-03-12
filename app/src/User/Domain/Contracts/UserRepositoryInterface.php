4de48b774f05a10145b8264bf8551f42d04efe34
Norivo7 <szymik.kamil97@gmail.com>
2026-03-12 09:53:32 +0100

    feat: extend UserRepositoryInterface contract

--- app/src/User/Domain/Contracts/UserRepositoryInterface.php
+++ app/src/User/Domain/Contracts/UserRepositoryInterface.php
@@ -8,7 +8,11 @@ use App\User\Domain\Entity\User;
 
 interface UserRepositoryInterface
 {
+    public function save(User $user): void;
+
     public function findById(int $id): ?User;
 
     public function findByEmail(string $email): ?User;
+
+    public function flush(): void;
 }
