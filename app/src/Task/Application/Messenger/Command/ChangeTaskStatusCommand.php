13789043941f3dbc7feb5be07f71f85b0863af94
Norivo7 <szymik.kamil97@gmail.com>
2026-03-12 09:57:02 +0100

    refactor: change task completion -> status change per new requirements

--- app/src/Task/Application/Messenger/Command/ChangeTaskStatusCommand.php
+++ app/src/Task/Application/Messenger/Command/ChangeTaskStatusCommand.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace App\Task\Application\Messenger\Command;
 
-final readonly class CompleteTaskCommand
+final readonly class ChangeTaskStatusCommand
 {
     public function __construct(
         public string $id,
+        public string $status,
     ) {
     }
 }
--- app/src/Task/Application/Messenger/Handler/ChangeTaskStatusHandler.php
+++ app/src/Task/Application/Messenger/Handler/ChangeTaskStatusHandler.php
@@ -4,23 +4,24 @@ declare(strict_types=1);
 
 namespace App\Task\Application\Messenger\Handler;
 
-use App\Task\Application\Messenger\Command\CompleteTaskCommand;
+use App\Task\Application\Messenger\Command\ChangeTaskStatusCommand;
 use App\Task\Domain\Contracts\TaskRepositoryInterface;
+use App\Task\Domain\Enums\TaskStatus;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
-final readonly class CompleteTaskHandler
+final readonly class ChangeTaskStatusHandler
 {
     public function __construct(
         private TaskRepositoryInterface $taskRepository,
     ) {
     }
 
-    public function __invoke(CompleteTaskCommand $command): void
+    public function __invoke(ChangeTaskStatusCommand $command): void
     {
         $task = $this->taskRepository->get($command->id);
 
-        $task->complete();
+        $task->changeStatus(TaskStatus::from($command->status));
 
         $this->taskRepository->save($task);
     }
